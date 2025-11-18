599d15df7c19e8babd7e137332e18ad7554e3e70
king <93720294+hungmaosong@users.noreply.github.com>
2025-11-18 15:57:14 +0800

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //for using cout
+using namespace std; //命名空間，不同空間的同名變數視為不同
+
+int main(){
+    std::cout << "Hello";
+    cout << "Fuck" << "NCKU" << endl;
+
+    return 0;
+}
+
+
+/*
+* g++ -o 執行檔名 CPP檔名.cpp
+* ./執行檔名
+*/
