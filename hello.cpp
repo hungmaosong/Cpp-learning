ecbc19f032d719abe8f023117668525574823a57
king <93720294+hungmaosong@users.noreply.github.com>
2025-11-18 16:04:56 +0800

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,7 @@ using namespace std; //命名空間，不同空間的同名變數視為不同
 
 int main(){
     std::cout << "Hello";
-    cout << "Fuck" << "NCKU" << endl;
+    cout << "Fuck" << "NCKU" << endl; //有加namespace，可不用寫std
 
     return 0;
 }
